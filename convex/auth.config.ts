ffef649a3c1e0ca6b9d381131a91e08443eac66d
ramemes <17809655+ramemes@users.noreply.github.com>
2024-10-14 00:59:27 +0100

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://correct-polecat-91.clerk.accounts.dev/",
+      domain: "https://factual-snail-92.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ]
